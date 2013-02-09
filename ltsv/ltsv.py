230abfbb176e2d4406836f84cd5f97621bd7c56a
INADA Naoki <inada-n@klab.com>
2013-02-09 11:15:57 +0900

    Remove self

--- ltsv/ltsv.py
+++ ltsv/ltsv.py
@@ -1,12 +1,12 @@
 # -*- encoding: utf-8 -*-
 
-def reader(self, obj, encoding="utf-8"):
+def reader(obj, encoding="utf-8"):
     if isinstance(obj, file):
         return self.parse_file(obj, encoding)
     else:
         return self.parse_string(obj, encoding)
 
-def writer(self, obj, lineterminator="\n", encoding="utf-8"):
+def writer(obj, lineterminator="\n", encoding="utf-8"):
     if isinstance(obj, dict):
         return self.dump(obj, encoding)
     elif isinstance(obj, list):
@@ -17,7 +17,7 @@ def writer(self, obj, lineterminator="\n", encoding="utf-8"):
     else:
         raise ValueError("not support")
 
-def parse_string(self, string, encoding="utf-8"):
+def parse_string(string, encoding="utf-8"):
     if isinstance(string, str):
         uni_string = string.decode(encoding).rstrip()
     elif isinstance(string, unicode):
@@ -26,10 +26,10 @@ def parse_string(self, string, encoding="utf-8"):
         raise ValueError("not support")
     return dict([x.split(u":", 1) for x in uni_string.split(u"\t")])
 
-def parse_file(self, handle, encoding="utf-8"):
+def parse_file(handle, encoding="utf-8"):
     return [self.parse_string(line, encoding) for line in handle]
 
-def dump(self, dic, encoding):
+def dump(dic, encoding):
     ltsv = u"\t".join(k + u":" + v for k, v in dic.iteritems())
     return ltsv.encode(encoding)
 
